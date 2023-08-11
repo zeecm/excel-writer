bd7be30275ce87fbf9048c02ba4d54f8696e4d24
zeecm <114894488+zeecm@users.noreply.github.com>
2023-08-11 08:47:30 +0000

    Implement acknowledgement form creator

---
+++ acknowledgement_form/generator.py
@@ -0,0 +1,71 @@
+import os
+from enum import Enum
+from typing import NamedTuple
+
+from excel_writer.writer import ExcelWriter, Writer
+
+TEMPLATE_FILEPATH = os.path.join(
+    "acknowledgement_form", "template", "template_job_ack.xlsx"
+)
+
+
+class FieldInfo(NamedTuple):
+    cell_id: str
+    template_str: str
+
+
+class Field(Enum):
+    CLIENT_NAME = FieldInfo("B3", "<client_name>")
+    JOB_NUM = FieldInfo("B4", "<job_num>")
+    PO_NUM = FieldInfo("B5", "<po_num>")
+    QUOTATION_NUM = FieldInfo("B6", "<quotation_num>")
+    VESSEL = FieldInfo("B7", "<vessel>")
+    DRAWING_NUM = FieldInfo("B11", "<drawing_num>")
+    CLASS = FieldInfo("B12", "<class>")
+    DURATION = FieldInfo("B13", "<duration>")
+
+
+def load_template(template_filepath: str = TEMPLATE_FILEPATH) -> ExcelWriter:
+    return ExcelWriter(template_filepath)
+
+
+def set_field_value(
+    writer: ExcelWriter, field: Field, value_to_set: str
+) -> ExcelWriter:
+    if not value_to_set:
+        value_to_set = "-"
+        if field == Field.CLASS:
+            value_to_set = "Not Involved"
+
+    field_cell_id = field.value.cell_id
+    field_template_str = field.value.template_str
+
+    client_name_cell = writer.cell(0, field_cell_id)
+
+    template_value = str(client_name_cell.value)
+
+    new_value = template_value.replace(field_template_str, value_to_set)
+    writer.cell(0, field_cell_id, new_value)
+
+    return writer
+
+
+def create_job_ack(
+    filename: str, filepath: str = ".", template_filepath: str = TEMPLATE_FILEPATH
+) -> None:
+    query_mapping = {}
+    for field in Field:
+        field_value = input(f"{field.name}: ")
+        query_mapping |= {field: field_value}
+
+    writer = load_template(template_filepath)
+
+    for field, value in query_mapping.items():
+        writer = set_field_value(writer, field, value)
+
+    writer.save_workbook(filepath, filename)
+
+
+if __name__ == "__main__":
+    filename = "test.xls"
+    create_job_ack(filename)
---
+++ tests/acknowledgement_form_tests/test_acknowledgement_form_generator.py
@@ -0,0 +1,36 @@
+import os
+
+import pytest
+
+from acknowledgement_form.generator import TEMPLATE_FILEPATH, Field, set_field_value
+from excel_writer.writer import ExcelWriter
+
+TEST_FILEPATH = os.path.join(
+    "tests", "acknowledgement_form_tests", "test_files", "template_job_ack.xlsx"
+)
+
+
+@pytest.mark.parametrize(
+    "field, value_to_set",
+    [
+        (Field.CLIENT_NAME, "abc pte ltd"),
+        (Field.PO_NUM, "123456"),
+        (Field.JOB_NUM, "2308001"),
+        (Field.QUOTATION_NUM, "MMSQ23-001"),
+        (Field.CLASS, "LR"),
+        (Field.VESSEL, "rss hello"),
+        (Field.DURATION, "2 days"),
+    ],
+)
+def test_set_field_value(field: Field, value_to_set: str):
+    writer = ExcelWriter(TEST_FILEPATH)
+    writer = set_field_value(writer, field, value_to_set)
+    cell_value = str(writer.cell(0, field.value.cell_id).value)
+    assert value_to_set in cell_value
+
+
+def test_empty_class_set_to_not_involved():
+    writer = ExcelWriter(TEST_FILEPATH)
+    writer = set_field_value(writer, Field.CLASS, "")
+    cell_value = str(writer.cell(0, Field.CLASS.value.cell_id).value)
+    assert "Not Involved" in cell_value
